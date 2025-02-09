7e69de8488800588b27e484c9ddae0d323176c2b
abhigya <abhigyadufare@gmail.com>
2025-02-09 07:18:09 -0500

    render venue items on UI

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -1,8 +1,25 @@
 import "./ConferenceEvent.css";
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrementQuantity, incrementQuantity } from "./venueSlice";
 
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
+    const venueItems = useSelector((state) => state.venue);
+    const dispatch = useDispatch();
+
+    const handleToggleItems = () => {
+        console.log("handleToggleItems called");
+        setShowItems(!showItems);
+    };
+
+    const handleAddToCart = (index) => {
+        dispatch(incrementQuantity(index));
+    };
+
+    const handleRemoveFromCart = (index) => {
+        dispatch(decrementQuantity(index));
+    }
 
     return (
         <>
@@ -26,7 +43,50 @@ const ConferenceEvent = () => {
                             <div className="text">
                                 <h1>Venue Room Selection</h1>
                             </div>
-                            <div className="venue_selection"></div>
+                            <div className="venue_selection">
+                                {venueItems.map((item, index) => (
+                                    <div className="venue_main" key={index}>
+                                        <div className="img">
+                                            <img src={item.img} alt={item.name} />
+                                        </div>
+                                        <div className="text">{item.name}</div>
+                                        <div>${item.cost}</div>
+                                        <div className="button_container">
+                                            {item.name === "Auditorium Hall (Capacity:200)" ? (
+                                                <>
+                                                    <button
+                                                        className={item.quantity === 0 ? "btn-warning btn-disabled" : "btn-minus btn-warning"}
+                                                        onClick={() => handleRemoveFromCart(index)}
+                                                    >
+                                                        &#8211;
+                                                    </button>
+                                                    <span>{item.quantity}</span>
+                                                    <button
+                                                        className={item.quantity === 3 ? "btn-success btn-disabled" : "btn-success btn-plus"}
+                                                        onClick={() => handleAddToCart(index)}
+                                                    >
+                                                        &#43;
+                                                    </button>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <button
+                                                        className={item.quantity === 0 ? "btn-warning btn-disabled" : "btn-warning btn-plus"}
+                                                        onClick={() => handleRemoveFromCart(index)}
+                                                    >
+                                                        &#8211;
+                                                    </button>
+                                                    <span>{item.quantity}</span>
+                                                    <button
+                                                        className={item.quantity === 10 ? "btn-success btn-disabled" : "btn-success btn-plus"}
+                                                        onClick={handleAddToCart(index)}
+                                                    ></button>
+                                                </>
+                                            )}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                             <div className="total_cost">Total Cost:</div>
                         </div>
                         <div id="addons" className="venue_container container_main">
